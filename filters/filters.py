a5f1d3bf127418a24ddb135577ad28d436e4aa0e
Chris Esprey <chris.esprey@senseye.io>
2018-07-23 22:26:52 +0100

    added a simple comb filter to the matched filter class and renamed to filters.

--- filters/filters.py
+++ filters/filters.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Matched Filter
+Filters 
 
 Created on Thu Jul  5 16:17:02 2018
 
@@ -11,9 +11,10 @@ from scipy.signal import lfilter as lfilt
 from scipy.signal import convolve as conv
 from scipy.signal import correlate as corr
 from matplotlib import pyplot as plt
+import scipy.signal as signal
 
 
-class Matched_Filter(object):
+class Filters:
 
     def matched_filter(self, matching_template, signal, method='lfilt'):
         """ Implementation of a matched filter, using either the FIR method
@@ -56,10 +57,30 @@ class Matched_Filter(object):
                                   method='auto')
 
         return matched_signal
+    
+    def comb_freqz(self, a, f0, fs):
+    
+        L = int(np.round(fs/f0))
+        h = np.zeros(L+1)   # fs
+        h[0] = 1
+        h[L] = a
+        
+        return h
+    
+    def comb_filt(self, x, f0, fs, a):
+    
+        h = self.comb_freqz(a, f0, fs)
+        x_comb = signal.lfilter(h, [1.0], x)
+        
+        return x_comb
+    
 
 if __name__ == '__main__':
-    """Find a single cycle sinusoid in random noise"""
-    match = Matched_Filter()
+    
+    """Matched Filter Example: Find a single cycle 
+    sinusoid in random noise"""
+    
+    filters = Filters()
 
     fs = 100    # sample rate, Hz
     f = 1       # Frequency of sinusoid, Hz
@@ -90,7 +111,7 @@ if __name__ == '__main__':
     methods = ['lfilt', 'conv', 'fftconv', 'corr']
 
     for method in methods:
-        matched_sig = match.matched_filter(sinusoid, noise_sig,
+        matched_sig = filters.matched_filter(sinusoid, noise_sig,
                                            method=method)
         if method is 'lfilt':
             plt.plot(matched_sig, linestyle='--', label=method)
@@ -99,3 +120,44 @@ if __name__ == '__main__':
 
     plt.show()
     plt.legend()
+    
+    """Comb Filter Example: Filter out 50 Hz Gaussian Pulse 
+    Train from random noise"""
+    
+    f0=50
+    fs = 8000
+    T=1/f0
+    fr=1500
+    
+    t=np.arange(0, T, 1/fs)
+    pulse = np.sin(2*np.pi*fr*t)
+    g = signal.gaussian(len(pulse), len(pulse)/20)
+    
+    pulse = pulse*g
+    x = []
+    
+    for n in np.arange(0,50):
+        
+        if n == 0:
+            x = np.append(x, np.zeros_like(pulse))        
+        x = np.append(x, pulse)
+    
+    # Add random noise
+    S = max(x)
+    SNR  = 30
+    snr = 10**(SNR/20)
+    noise = S/snr
+    
+    x_noise = x + np.random.normal(0, noise, len(x))
+    
+    x_comb = filters.comb_filt(x_noise, f0, fs, -1)
+    
+    plt.figure()
+    plt.plot(x_noise)
+    plt.plot(x_comb)
+    plt.legend(['Gauss Pulse Train', 'CombFiltered'])
+    plt.xlabel('sample number')
+    plt.ylabel('amplitude')
+    
+    
+    
